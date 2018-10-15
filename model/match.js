62cb429b9d1080c2cbe48a461bf2fc699157ec26
Daniel De Gaspari <daniel_DG@hotmail.it>
2018-10-15 15:23:40 +0200

    Added player and match models

---
+++ model/match.js
@@ -0,0 +1,19 @@
+// A model for match with params: playerList and time
+class Match {
+
+    /**
+     * @param {*} playerList A list of players
+     * @param {*} time timestamp
+     */
+    // this.paramName = paramName;
+    constructor(playerList, time) {
+    }
+
+    getPlayerList() {
+    }
+
+    getTime() {
+    }
+}
+
+module.exports = Match;
---
+++ model/player.js
@@ -0,0 +1,17 @@
+ // A model for player with params: name
+ class Player {
+
+    /**
+     * 
+     * @param {*} name player
+     */
+    // this.paramName = paramName;
+    constructor(name) {
+    }
+
+    getName() {
+    }
+    
+}
+
+module.exports = Player;
